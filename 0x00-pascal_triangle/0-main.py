97853c4df4e6089ebf5d296969a87317a0e26150
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-03 02:31:09 +0300

    t0r2

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -11,4 +11,4 @@ def print_triangle(triangle):
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
 if __name__ == "__main__":    
-   print_triangle(pascal_triangle(10))
+   print_triangle(pascal_triangle(-1))
--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,10 +8,14 @@ def pascal_triangle(n):
     create the triangle
     """
     result = [[1]]
-    for x in range(n - 1):
-        resultTemp = [0] + result[-1] + [0]
-        newRow = []
-        for y in range(len(result[-1]) + 1):
-            newRow.append(resultTemp[y] + resultTemp[y + 1])
-        result.append(newRow)
-    return result
+    if (n >= 0):
+        for x in range(n - 1):
+            resultTemp = [0] + result[-1] + [0]
+            newRow = []
+            for y in range(len(result[-1]) + 1):
+                newRow.append(resultTemp[y] + resultTemp[y + 1])
+            result.append(newRow)
+        return result
+    else:
+        result = []
+        return result
