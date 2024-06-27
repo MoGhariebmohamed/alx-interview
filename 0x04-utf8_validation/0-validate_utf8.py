ced653404ced0a2b8719728a8deabf743c057c52
Mo Gharieb <m.gharieb89@gmail.com>
2024-06-27 09:51:47 +0300

    t0v5

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,7 +14,7 @@ def validUTF8(data):
         if ignore > 0:
             ignore -= 1
             continue
-        if type(data[x]) != int or data[x] < 0 or data[x] > 0x10ffff: 
+        if type(data[x]) != int or data[x] < 0 or data[x] > 0x10ffff:
             return False
         elif data[x] < 0x7f:
             ignore = 0
