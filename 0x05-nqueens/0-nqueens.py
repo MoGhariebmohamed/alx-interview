38c43a9cf7d7f54e30baef570f7f1c75aac27879
Mo Gharieb <m.gharieb89@gmail.com>
2024-07-08 01:32:52 +0300

    t0 ver 1

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+N queens puzzle is the challenge of placing N non-attacking queen
+"""
+import sys
+
+
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print ("Usage: nqueens N")
+    exit(1)
+
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1) 
+
+if int(sys.argv[1]) < 4:
+    print ("N must be at least 4")
+    exit(1) 
+
+countn = int(sys.argv[1])
+
+
+def queen_pos(countn, x=0, a=[], b=[], c=[]):
+    """
+    to get the possible positions
+    """
+    if x < countn:
+        for any in range(countn):
+            if any not in a and x + any not in b and x - any not in c:
+                yield from queen_pos(countn, x + 1, a + [any], b + [x + any], c + [x - any])
+
+            else:
+                yield a
+
+
+def solution(countn):
+    """
+    to solve the location of n
+    """
+    x = []
+    y = 0
+    for any in queen_pos(countn, 0):
+        x.append([y, any])
+        y += 1
+        print(x)
+        x = []
+        y = 0
+
+
+solution(countn)
