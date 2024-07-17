eeca7b6a2efaf1d59b5ca11a345f5aedecaf3c41
Mhamed Gharieb <m.gharieb89@gmail.com>
2024-07-18 01:31:46 +0300

    t2v3

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -11,6 +11,7 @@ def rotate_2d_matrix(matrix):
     length_mat = len(matrix)
     for any in range(length_mat):
         for nex in range(any, length_mat):
-            matrix[any][nex], matrix[nex][any] = matrix[nex][any], matrix[any][nex]
+            matrix[any][nex], matrix[nex][any] = \
+                matrix[nex][any], matrix[any][nex]
     for initial in matrix:
         initial.reverse()
