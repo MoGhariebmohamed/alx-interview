c2680156d0050f92aee00ebd2cdf63169836e1f4
Mo Gharieb <m.gharieb89@gmail.com>
2024-08-02 04:38:32 +0300

    task 0 rev0

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+calculatee the  the perimeter of the island described in grid
+"""
+
+
+def island_perimeter(grid):
+    """
+    Return: the perimeter of the island described in grid
+    """
+    x = 0
+    for any in range(len(grid)):
+        for num in range(len(grid[any])):
+            if (grid[any][num] == 1):
+                if (any <= 0 or grid[any - 1][num] == 0):
+                    x += 1
+                if (any >= len(grid) - 1 or grid[any + 1][num] == 0):
+                    x +=1
+                if (num <= 0 or grid[any][num - 1] == 0):
+                    x += 1
+                if (num >= len(grid[any]) - 1 or grid[any][num + 1] == 0):
+                    x +=1
+    return x
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
